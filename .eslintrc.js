2d5ea90e2a60f2644f6e40fd7b7a8a445ae53abf
Marcelo Ragnelli Frigério <marcelo_ragnelli@MacBook-Pro-de-Marcelo.local>
2019-11-21 09:55:25 -0300

    Criada a estrutura inicial do projeto

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb-base',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly'
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+    'prettier',
+    'react-hooks',
+    'import',
+    'jsx-a11y'
+  ],
+  rules: {
+    'comma-dangle': ["error", "never"],
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [
+      'error',
+      { extensions: ['.jsx', '.js'] }
+    ],
+    'class-methods-use-this': 'off',
+    'import/prefer-default-export': 'off',
+    'no-console': ['error', { allow: ['tron'] }],
+    'no-unused-vars': ['error', { 'argsIgnorePattern': 'next' }],
+    'no-param-reassign': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'camelcase': 'off',
+    'no-underscore-dangle': 'off',
+    'react-native/no-raw-text': 'off',
+    'react/jsx-one-expression-per-line': 'off',
+    'no-unused-vars': "off"
+  },
+};
